ad02ea0a28411183a17956ca32a0381c0c9fdc3e
shiv <gadedshiv@gmail.com>
2022-03-20 20:37:12 +0530

    stream practice

---
+++ Java 8 Streams/src/StreamPractice.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> numbers=List.of(1,2,3,4,5,6,1,2,7,8,9,10);
+		
+		System.out.println(numbers.stream().distinct().collect(Collectors.toList()));
+		System.out.println(numbers.stream().sorted().collect(Collectors.toList()));
+	}
+
+}
